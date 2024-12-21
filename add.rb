5d364ef8ba4dda6063f4afa227f5337a28794a0b
Naresh Dwivedi <naresh.dwivedi1987@gmail.com>
2024-12-21 16:28:07 +0530

    Update add method to allow specifying delimiter in argument

--- add.rb
+++ add.rb
@@ -1,8 +1,13 @@
 def add(numbers)
   return 0 if numbers == ''
 
-  numbers = numbers.gsub("\n", ",")
-  numbers = numbers.split(',').map(&:to_i)
-
+  delimiter = ','
+  if numbers.start_with?('//')
+    delimiter = numbers[2]
+    numbers = numbers[3..-1]
+  else
+    numbers = numbers.gsub("\n", ",")
+  end
+  numbers = numbers.split(delimiter).map(&:to_i)
   numbers.sum
 end
