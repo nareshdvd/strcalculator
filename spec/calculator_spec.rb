3d6aebe7b4e419c75d70bc2d07ea855abba5fad3
Naresh Dwivedi <naresh.dwivedi1987@gmail.com>
2024-12-21 15:31:43 +0530

    Added spec to check if method returns sum of numbers provided comma separated, which fails as the method is not updated yet to do that

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe "#add" do
       expect(add('45')).to eq(45)
     end
   end
+
+  context "with any amount of numbers provided with , as delimiter" do
+    it 'retuns sum of the numbers provided' do
+      expect(add('30,40')).to eq(70)
+    end
+  end
 end
